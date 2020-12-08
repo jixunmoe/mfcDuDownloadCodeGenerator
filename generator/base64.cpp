6768d82952bb98ccf18691a7ec810e35176e0133
Jixun <jixun.moe@gmail.com>
2020-12-08 22:44:57 +0000

    改造 base64 代码，使其输出 wchar

--- generator/base64.cpp
+++ generator/base64.cpp
@@ -15,7 +15,7 @@
 #include "stdafx.h"
 #include "base64.h"
 
-static const unsigned char base64_table[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+static const wchar_t base64_table[] = L"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 /**
 * base64_encode - Base64 encode
@@ -25,9 +25,8 @@ static const unsigned char base64_table[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefg
 * Returns:  Allocated buffer of out_len bytes of encoded data,
 *           or empty string on failure
 */
-std::string base64_encode(const unsigned char* src, size_t len)
+wchar_t* base64_encode(const unsigned char* src, size_t len)
 {
-    unsigned char* out, * pos;
     const unsigned char* end, * in;
 
     size_t olen;
@@ -35,15 +34,13 @@ std::string base64_encode(const unsigned char* src, size_t len)
     olen = 4 * ((len + 2) / 3); /* 3-byte blocks to 4-byte */
 
     if (olen < len)
-        return std::string(); /* integer overflow */
+        return nullptr;
 
-    std::string outStr;
-    outStr.resize(olen);
-    out = (unsigned char*)&outStr[0];
+    wchar_t* out = (wchar_t*)calloc(olen + 1, sizeof(wchar_t));
 
     end = src + len;
     in = src;
-    pos = out;
+    auto pos = out;
     while (end - in >= 3) {
         *pos++ = base64_table[in[0] >> 2];
         *pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
@@ -56,15 +53,15 @@ std::string base64_encode(const unsigned char* src, size_t len)
         *pos++ = base64_table[in[0] >> 2];
         if (end - in == 1) {
             *pos++ = base64_table[(in[0] & 0x03) << 4];
-            *pos++ = '=';
+            *pos++ = L'=';
         }
         else {
             *pos++ = base64_table[((in[0] & 0x03) << 4) |
                 (in[1] >> 4)];
             *pos++ = base64_table[(in[1] & 0x0f) << 2];
         }
-        *pos++ = '=';
+        *pos++ = L'=';
     }
 
-    return outStr;
+    return out;
 }
--- generator/base64.h
+++ generator/base64.h
@@ -1,4 +1,4 @@
 #pragma once
 #include <string>
 
-std::string base64_encode(const unsigned char* src, size_t len);
+wchar_t* base64_encode(const unsigned char* src, size_t len);
