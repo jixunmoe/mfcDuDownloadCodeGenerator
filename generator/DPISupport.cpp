c79f6c78e0b4a91d04f00443651a8145935349f8
Jixun Wu <jixun.moe@gmail.com>
2022-10-16 17:24:49 +0100

    ci: get xp build to work again (+ code reformat)?

--- generator/DPISupport.cpp
+++ generator/DPISupport.cpp
@@ -1,30 +1,29 @@
-#include "stdafx.h"
 #include "DPISupport.h"
+#include "stdafx.h"
 
-#if !BUILD_FOR_XP
-#include <VersionHelpers.h>
+#if !__BUILD_FOR_XP
 #include <ShellScalingApi.h>
+#include <VersionHelpers.h>
 
-#pragma comment( lib, "Shcore" )
+#pragma comment(lib, "Shcore")
 #endif
 
-uint32_t DPISupport::GetWindowDPI(HWND hWnd)
-{
-    UINT xdpi = 0;
-    UINT ydpi = 0;
+uint32_t DPISupport::GetWindowDPI(HWND hWnd) {
+  UINT xdpi = 0;
+  UINT ydpi = 0;
 
-#if !BUILD_FOR_XP
-    // Windows 8.1 or higher
-    if (IsWindows8Point1OrGreater())
-    {
-        HMONITOR hMonitor = MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST);
-        LRESULT success = GetDpiForMonitor(hMonitor, MDT_EFFECTIVE_DPI, &xdpi, &ydpi);
-        return success == S_OK ? ydpi : 96;
-    }
+#if !__BUILD_FOR_XP
+  // Windows 8.1 or higher
+  if (IsWindows8Point1OrGreater()) {
+    HMONITOR hMonitor = MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST);
+    LRESULT success =
+        GetDpiForMonitor(hMonitor, MDT_EFFECTIVE_DPI, &xdpi, &ydpi);
+    return success == S_OK ? ydpi : 96;
+  }
 #endif
 
-    HDC hDC = GetDC(hWnd);
-    ydpi = static_cast<UINT>(GetDeviceCaps(hDC, LOGPIXELSY));
-    ReleaseDC(hWnd, hDC);
-    return ydpi;
+  HDC hDC = GetDC(hWnd);
+  ydpi = static_cast<UINT>(GetDeviceCaps(hDC, LOGPIXELSY));
+  ReleaseDC(hWnd, hDC);
+  return ydpi;
 }
